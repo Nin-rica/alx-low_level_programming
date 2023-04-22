377d32cfe90e4f97e9fba424cf8432856b57be7f
nin-rica <ninrica97@gmail.com>
2023-04-22 04:38:58 +0100

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  * main - Print all single digit
+ *   * with putchar function
+ *    * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return 0;
+}
