1b238685999cd10f4d040277c28b8e88018351a9
nin-rica <ninrica97@gmail.com>
2023-04-22 05:00:35 +0100

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *  * main - Write a program that prints
+ *   * all possible different combinations
+ *    * of two digits.
+ *     * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+
+		if ((i == 56) && (j == 57))
+		continue;
+
+	putchar(44);
+	putchar(32);
+		}
+	}
+	putchar('\n');
+	return 0;
+}
+
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *  * main - Write a program that
+ *   * prints all possible combinations
+ *    * of two two-digit numbers.
+ *     * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+		        putchar(32);
+			putchar((j / 10) + 48);
+		        putchar((j % 10) + 48);
+			if ((i == 98) && (j == 99))
+				continue;
+		        putchar(44);
+	                putchar(32);
+		}
+	}
+	putchar('\n');
+	return 0;
+}
